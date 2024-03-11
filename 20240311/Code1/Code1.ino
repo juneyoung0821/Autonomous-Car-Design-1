15ec13316a7d51990ab0aec6c150357985c2ad22
juneyoung0821 <160203302+juneyoung0821@users.noreply.github.com>
2024-03-11 09:43:31 +0900

    Add files via upload

---
+++ 20240311/Code1/Code1.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 설정함수
+Serial.begin(9600); //통신 속도를 9600으로 설정
+  //Serial.println("test!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 실행 함수
+  Serial.println("test!");
+}
