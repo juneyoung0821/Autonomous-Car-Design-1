215b06cd9acc3ef902b1e3b3f00fdedd9fda65b7
juneyoung0821 <160203302+juneyoung0821@users.noreply.github.com>
2024-03-11 09:51:43 +0900

    Code2

---
+++ 20240311/Code1/Code2.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 설정함수
+  Serial.begin(9600);
+  Serial.print("Hello World!!");
+  Serial.print("Hello World!!");
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
