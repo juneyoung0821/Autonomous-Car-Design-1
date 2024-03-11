b52d918d3ceec0c41d994dc09f13dd54dd216c2f
juneyoung0821 <160203302+juneyoung0821@users.noreply.github.com>
2024-03-11 19:05:52 +0900

    Code3

---
+++ 20240311/Code1/Code3/Code3.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  // 설정함수
+  Serial.begin(9600);
+  Serial.println("Hello World!!");
+  Serial.println("Hello World!!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
